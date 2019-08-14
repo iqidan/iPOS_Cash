c3fc49b0e1078bc8c2b043f990db7cdbf24ee7d8
wenjie.geng <gwjlib@163.com>
2019-08-14 16:15:08 +0800

    搜索组件优化解耦方式&收银页面部分优化

--- src/components/_search.vue
+++ src/components/_search.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="search">
         <div class="filters" @click.capture="popupVisible = true">
-            <span class="line1">{{ checked }}</span>
+            <span class="line1">{{ checkedItem.label }}</span>
             <i class="triangle"></i>
 
             <mt-popup class="filter-list" v-model="popupVisible">
@@ -25,9 +25,7 @@
 </template>
 
 <script>
-import api from '@/api';
 import { Radio, Popup, Field, MessageBox } from 'mint-ui';
-import { mapMutations, mapGetters, mapState } from 'vuex';
 
 export default {
     name: 'Search',
@@ -47,159 +45,28 @@ export default {
             default: '请输入搜索内容'
         }
     },
-    computed: {
-        ...mapState({
-            selected_brand: state => state.shopConfig.selected_brand,
-            selected_guide: state => state.shopConfig.selected_guide,
-            shop_code: state => state.shopConfig.shop_config.shop_code,
-            selected_vip: state => state.vip.selected_vip,
-            update_vip_list: state => state.shopConfig.shop_config.update_vip
-        }),
-        ...mapGetters(['getSearchTypeValue'])
-    },
     data() {
         return {
-            checked: this.$store.state.search.selected_search_type,
+            checked: this.selectList[0].value,
             keyWords: '',
-            popupVisible: false,
-            goods_cart: {
-                goods: []
-            }
+            popupVisible: false
         };
     },
+    computed: {
+        checkedItem() {
+            const checkedItem = this.selectList.find(item => item.value === this.checked);
+            return checkedItem || {};
+        }
+    },
     methods: {
-        ...mapMutations(['setType', 'set_selected_vip']),
         // enter搜索事件
         keyUp() {
-            switch (this.getSearchTypeValue) {
-                case '1':
-                    this.searchGoods();
-                    break;
-                case '2':
-                    this.searchVip();
-                    break;
-                case '3':
-                    this.searchCoupon();
-                    break;
-            }
+            this.$emit('search', this.keyWords);
         },
-        searchGoods() {
-            return api
-                .search_goods({ sptm: this.keyWords })
-                .then(res => {
-                    if (res.data.data && res.data.data[0]) {
-                        if (res.data.data[0].ppdm !== this.selected_brand.brand.code) {
-                            this.$toast(`不允许录入,非${this.selected_brand.brand.name}品牌商品!`);
-                            return false;
-                        }
-                        return res.data.data[0];
-                    }
-                })
-                .then(res => {
-                    if (!res) {
-                        return false;
-                    }
-
-                    return api
-                        .search_goods_by_id({
-                            sptm: res.singleProductCode,
-                            shop_code: this.shop_code,
-                            kw_id: 1
-                        })
-                        .then(res2 => {
-                            if (res2.data.data && res2.data.data[0]) {
-                                if (!res2.data.data[0].kcsl || res2.data.data[0].kcsl < 1) {
-                                    this.$toast('该商品库存不足!');
-                                    return false;
-                                }
-                                //库存数量判断
-                                let sum = 0;
-                                this.goods_cart.goods.forEach(good => {
-                                    if (good.singleProductCode === res.singleProductCode) {
-                                        sum += good.sl;
-                                    }
-                                });
-                                if (sum >= res2.data.data[0].kcsl) {
-                                    this.$toast('超出最大库存数,不允许添加!');
-                                    return;
-                                }
-
-                                res.kcsl = res2.data.data[0].kcsl;
-                                return res;
-                            }
-                        });
-                })
-                .then(res => {
-                    if (!res) {
-                        return;
-                    }
-                    res.init_price = res.dj;
-                    if (res.sl === undefined) {
-                        res.sl = 1;
-                    }
-                    res.guide = this.selected_guide;
-                    // this.goods_cart.goods.push(res);
-                    // this.goodsCart.refersh_cart();
-                    this.$emit('search', res);
-                });
-        },
-        searchVip() {
-            const keyword = this.keyWords;
-            if (!keyword || keyword.length < 3) {
-                this.$toast('请输入会员号!');
-                return;
-            }
-            return api
-                .search_vip({
-                    vip_code: keyword,
-                    wkdm: '',
-                    is_select_vip: '',
-                    source: '6'
-                })
-                .then(res => {
-                    let is_vip_of_current_brand = false; // 是当前品牌vip
-                    if (res.status === 1) {
-                        let member = res.data;
-                        is_vip_of_current_brand = member.brands.some(v => {
-                            if (v.brandid === this.selected_brand.brand.code) {
-                                this.selected_brand.customer_level = v.loyaltytieridname;
-                                this.selected_brand.customer_level_id = v.loyaltytierid;
-                                member.membercard = v.membercard;
-                                member.customer_level = v.loyaltytieridname;
-                                return true;
-                            }
-                            return false;
-                        });
-                        if (is_vip_of_current_brand) {
-                            let typeList = [{ code: '00', name: '普通会员' }];
-                            if (member.ifstaff) {
-                                typeList.push({ code: '10', name: '员工' });
-                            }
-                            if (member.svip) {
-                                typeList.push({ code: '20', name: 'SVIP' });
-                            }
-                            member.typeList = typeList;
-                            member.type = typeList[0];
-                            this.set_selected_vip(member);
-                            this.vip_check(member);
-                        } else {
-                            this.$toast(
-                                '该会员不属于' +
-                                    this.selected_brand.brand.name +
-                                    '品牌会员!'
-                            );
-                        }
-                    } else {
-                        this.$toast('未查找到该会员');
-                    }
-                });
-        },
-        searchCoupon() {
-            return api.search_coupons({ coupon_code: this.keyWords });
-        },
+        // 搜索框左侧的搜索维度
         selectFilter() {
             this.popupVisible = false;
-            this.setType(this.checked);
+            this.$emit('filterChange', this.checkedItem);
         },
         //检查vip相关信息
         vip_check(vip) {
--- src/views/Cashier/cashier.vue
+++ src/views/Cashier/cashier.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="cashier">
-        <!-- <search-header
-            @search="searchGoods"
+        <search-header
+            @search="onSearch"
+            @filterChange="changeFilter"
             :select-list="search_type_list"
-            :placeholder="'搜索商品'"/> -->
+            :placeholder="'搜索商品'"/>
 
         <!-- 中心内容 start -->
         <div class="cashier-content">
@@ -154,21 +155,19 @@ export default {
             edittedGoodPrice: '',
             totalNum: 0,
             totalMoney: 0,
+            saerchType: null,
             search_type_list: [
                 {
                     label: '商品',
-                    value: '商品',
-                    unique: '1'
+                    value: '1'
                 },
                 {
                     label: '会员',
-                    value: '会员',
-                    unique: '2' 
+                    value: '2'
                 },
                 {
                     label: '优惠券',
-                    value: '优惠券',
-                    unique: '3'
+                    value: '3'
                 }
             ]
         };
@@ -196,6 +195,9 @@ export default {
     },
     methods: {
         ...mapMutations(['set_selected_guide', 'set_selected_brand']),
+        changeFilter(filter) {
+            console.log(filter);
+        },
         // 弹框显示
         showActionPopup(msg) {
             this.actionVisiable = true;
@@ -223,16 +225,152 @@ export default {
             this.set_selected_guide(guide);
             this.$refs.guidePopup.turn();
         },
-        // 搜索商品
-        searchGoods(good) {
-            const goodInCartIndex = this.goodsCart.goods.findIndex(g => g.sku === good.sku);
-            if (goodInCartIndex >= 0) {
-                this.goodsCart.goods[goodInCartIndex].sl++
-            } else {
-                this.goodsCart.goods.push(good);
+        onSearch(keyWords) {
+            switch (this.saerchType) {
+                case '1':
+                    this.searchGoods(keyWords).then(res => {
+                        console.log(res);
+                        return res;
+                    });
+                    break;
+                case '2':
+                    this.searchVip(keyWords).then(res => {
+                        console.log(res);
+                        return res;
+                    });
+                    break;
+                case '3':
+                    this.searchCoupon(keyWords).then(res => {
+                        console.log(res);
+                        return res;
+                    });
+                    break;
             }
-            this.refreshCart();
         },
+        searchGoods() {
+            return api
+                .search_goods({ sptm: this.keyWords })
+                .then(res => {
+                    if (res.data.data && res.data.data[0]) {
+                        if (res.data.data[0].ppdm !== this.selected_brand.brand.code) {
+                            this.$toast(`不允许录入,非${this.selected_brand.brand.name}品牌商品!`);
+                            return false;
+                        }
+                        return res.data.data[0];
+                    }
+                })
+                .then(res => {
+                    if (!res) {
+                        return false;
+                    }
+
+                    return api
+                        .search_goods_by_id({
+                            sptm: res.singleProductCode,
+                            shop_code: this.shop_code,
+                            kw_id: 1
+                        })
+                        .then(res2 => {
+                            if (res2.data.data && res2.data.data[0]) {
+                                if (!res2.data.data[0].kcsl || res2.data.data[0].kcsl < 1) {
+                                    this.$toast('该商品库存不足!');
+                                    return false;
+                                }
+                                //库存数量判断
+                                let sum = 0;
+                                this.goods_cart.goods.forEach(good => {
+                                    if (good.singleProductCode === res.singleProductCode) {
+                                        sum += good.sl;
+                                    }
+                                });
+                                if (sum >= res2.data.data[0].kcsl) {
+                                    this.$toast('超出最大库存数,不允许添加!');
+                                    return;
+                                }
+
+                                res.kcsl = res2.data.data[0].kcsl;
+                                return res;
+                            }
+                        });
+                })
+                .then(res => {
+                    if (!res) {
+                        return;
+                    }
+                    res.init_price = res.dj;
+                    if (res.sl === undefined) {
+                        res.sl = 1;
+                    }
+                    res.guide = this.selected_guide;
+                    // this.goods_cart.goods.push(res);
+                    // this.goodsCart.refersh_cart();
+                    this.$emit('search', res);
+                });
+        },
+        searchVip() {
+            const keyword = this.keyWords;
+            if (!keyword || keyword.length < 3) {
+                this.$toast('请输入会员号!');
+                return;
+            }
+            return api
+                .search_vip({
+                    vip_code: keyword,
+                    wkdm: '',
+                    is_select_vip: '',
+                    source: '6'
+                })
+                .then(res => {
+                    let is_vip_of_current_brand = false; // 是当前品牌vip
+                    if (res.status === 1) {
+                        let member = res.data;
+                        is_vip_of_current_brand = member.brands.some(v => {
+                            if (v.brandid === this.selected_brand.brand.code) {
+                                this.selected_brand.customer_level = v.loyaltytieridname;
+                                this.selected_brand.customer_level_id = v.loyaltytierid;
+                                member.membercard = v.membercard;
+                                member.customer_level = v.loyaltytieridname;
+                                return true;
+                            }
+                            return false;
+                        });
+                        if (is_vip_of_current_brand) {
+                            let typeList = [{ code: '00', name: '普通会员' }];
+                            if (member.ifstaff) {
+                                typeList.push({ code: '10', name: '员工' });
+                            }
+                            if (member.svip) {
+                                typeList.push({ code: '20', name: 'SVIP' });
+                            }
+                            member.typeList = typeList;
+                            member.type = typeList[0];
+                            this.set_selected_vip(member);
+                            this.vip_check(member);
+                        } else {
+                            this.$toast(
+                                '该会员不属于' +
+                                    this.selected_brand.brand.name +
+                                    '品牌会员!'
+                            );
+                        }
+                    } else {
+                        this.$toast('未查找到该会员');
+                    }
+                });
+        },
+        searchCoupon() {
+            return api.search_coupons({ coupon_code: this.keyWords });
+        },
+        // 添加商品到购物车
+        // searchGoods(good) {
+        //     const goodInCartIndex = this.goodsCart.goods.findIndex(g => g.sku === good.sku);
+        //     if (goodInCartIndex >= 0) {
+        //         this.goodsCart.goods[goodInCartIndex].sl++
+        //     } else {
+        //         this.goodsCart.goods.push(good);
+        //     }
+        //     this.refreshCart();
+        // },
         // 显示编辑弹框
         showGoodEditPannel(good) {
             this.edittedGoodPrice = '';
